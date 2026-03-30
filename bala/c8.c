9aa37406da71ec0b7c09645bb88fe00dc65bc65f
balamoorthy902bee27 <balamoorthy902_bee27@mepcoeng.ac.in>
2026-03-30 12:22:15 +0530

    Create c8.c

---
+++ bala/c8.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+int main() {
+    int n, sum = 0;
+    if (scanf("%d", &n) != 1) return 1;
+    while (n > 0) {
+        sum += (n % 10); 
+        n /= 10;         
+    }
+    printf("%d\n", sum);
+    return 0;
+}
+
